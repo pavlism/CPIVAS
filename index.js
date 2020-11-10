d333749b8c28eef47dc64b0b2d3c67af00396b68
pavlism <pavlismark@gmail.com>
2020-11-10 12:36:39 -0500

    asd

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+
+
+/*
 const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World2!");
+    response.end("Hello World3!");
 });
 
 const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+*/
+
+'use strict';
+
+// [START gae_node_request_example]
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
+
+app.use(express.static(__dirname + '/public')); //__dir and not _dir
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json({limit:'1mb'}));
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
+
+var fs = require('fs');
